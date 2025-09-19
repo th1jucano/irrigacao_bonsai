2bf8ca9a99f0871615e536da821cf1e12a03f0ea
Thiago Lopes <thiago.corporative@gmail.com>
2025-09-19 14:19:08 -0300

    Adicionados led de status e timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <Arduino.h>
+#include <RTClib.h>
 
 #define bomba 5
-#define led 13
 #define sensor A0
+#define led 13
+
+RTC_DS3231 rtc;
+
+unsigned long pisca = 0;
+bool estadoLed = false;
+int umidade = 0;
+
+void piscaLed(){
+
+  unsigned long agoraMillis = millis();
+
+  if (estadoLed && (agoraMillis - pisca >= 1000)){
+    digitalWrite(led, LOW);
+    estadoLed = false;
+    pisca = agoraMillis;
+  }else if(!estadoLed && (agoraMillis - pisca >= 500)){
+    digitalWrite(led, HIGH);
+    estadoLed = true;
+    pisca = agoraMillis;
+  }
+
+}
 
 void ligaBomba(){ // Ligar a bomba 
   digitalWrite(bomba, HIGH);
@@ -18,20 +41,34 @@ int lerSensor(){ // Leitura do sensor
 
 void setup() {
   Serial.begin(9600);
+  rtc.begin();
   pinMode(bomba, OUTPUT);
   pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
 }
 
 void loop() {
-  if (lerSensor() > 670 ){ // Até agora o número razoável de umidade do solo, alterar quando necessário
-    ligaBomba();
-  }else{
-    desligaBomba();
+  DateTime agora = rtc.now();
+  int horaAtual = agora.hour(); // data e hora atuais
+  umidade = lerSensor();
+
+  piscaLed();
+  
+  if ((horaAtual > 7) && (horaAtual < 17)){
+    Serial.print(" ");
+    Serial.println(horaAtual);
+    if (umidade > 670 ){ // Até agora o número razoável de umidade do solo, alterar quando necessário
+      ligaBomba();
+    }else{
+      desligaBomba();
+    }
   }
-  delay(1000);
 
-  Serial.println(lerSensor());
-  delay(1000);
-
+  Serial.println(umidade);
+  
+  
 }
 
